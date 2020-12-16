fb2a90adeb14f5fdae99c591fd0e0ee630b23861
jaeuk2274 <jaeuk2274@gmail.com>
2020-12-16 16:18:17 +0900

    Update Lv2_스택큐_기능개발.java

--- src/me/jaeuk/programmers/Lv2_스택큐_기능개발.java
+++ src/me/jaeuk/programmers/Lv2_스택큐_기능개발.java
@@ -16,8 +16,29 @@ public class Lv2_스택큐_기능개발 {
     }
 
     public int[] solution(int[] progresses, int[] speeds){
-        int[] answer = new int[0];
+        int[] answerLen = new int[progresses.length];
+        for(int i=0; i<progresses.length; i++) {
+        	answerLen[i] = (int)Math.ceil((float)(100-progresses[i])/speeds[i]);
+        }
 
+        List answerList = new ArrayList<Integer>();
+        int max = answerLen[0]; 
+        int count = 0;
+        for(int i=0; i<answerLen.length; i++) {
+        	if(max >= answerLen[i]) {
+        		count++;
+        	} else {
+        		answerList.add(count);
+        		count = 1;
+        		max = answerLen[i];
+        	}
+        }
+        
+        answerList.add(count);      
+        int[] answer = new int[answerList.size()];
+        for(int i=0; i<answerList.size(); i++) {
+        	answer[i] = (int) answerList.get(i);
+        }
         return answer;
     }
 }
